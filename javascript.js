296d79aa4923939892992f0cb2341b35cc51a9f6
chaotix-nld <189079957+chaotix-nld@users.noreply.github.com>
2025-01-08 15:08:16 +0100

    added some images for the array created array in javascript Filled array with dummy content(objects) added folder for images added stock image for uploads

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
+const myLibrary =[
+    {title: "The 3 body problem" , author:"Liu Cixin", pageNum: 416, cover: `img.src="images/threeBody.jpg"`},
+    {title: "The Dark Forest" , author:"Liu Cixin", pageNum: 518, cover: `img.src="images/darkForest.jpg"`},
+    {title: "Deaths End" , author:"Liu Cixin", pageNum: 592, cover: `img.src="images/deathsEnd.jpg"`},
+    {title: "Wizards First Rule" , author:"Terry Goodkind", pageNum: 836, cover: `img.src="images/Wizard's_First_Rule.jpg"`},
+    {title: "Stone Of Tears" , author:"Terry Goodkind", pageNum: 979, cover: `img.src="images/Stone_of_Tears.jpg"`},
+    {title: "Blood Of The Fold" , author:"Terry Goodkind", pageNum: 623, cover: `img.src="images/Blood_of_the_Fold.jpg"`},
+] 
+
+
+
+
+
+
+
+
+
+
+
+
+
 //elements for modal//
 const modal = document.getElementById("modalTotal");
 const addBook = document.getElementById("newBook");
